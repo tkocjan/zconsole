1a2f4c1b036aaa9f93e9589728271c0b41ca397e
Thomas Kocjan <thomas.kocjan@gmail.com>
2013-05-28 20:12:51 -0600

    Just testing.

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ set_include_path(implode(PATH_SEPARATOR, array(
     get_include_path(),
 )));
 
-/* Zend_Application */
+/** Zend_Application */
 require_once 'Zend/Application.php';
 
 // Create application, bootstrap, and run
