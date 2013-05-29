b0884842ae6a89e1a729b02de3f60aa1d5e02db6
Thomas Kocjan <thomas.kocjan@gmail.com>
2013-05-28 19:16:05 -0600

    Logging APPLICATION_ENV.

--- public/index.php
+++ public/index.php
@@ -9,6 +9,8 @@ defined('APPLICATION_PATH')
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
+error_log('APPLICATION_ENV='.APPLICATION_ENV);
+
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
